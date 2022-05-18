41a4b9bf0c5fe31205a82ae3e75ad178bba14feb
dhsiao510 <dhsiao510@gmail.com>
2022-05-17 17:09:48 -0700

    mvp without media

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
+import "./App.css"
+
 function App() {
   return (
     <div className="App">
-      <h1>David Hsiao</h1>
+      <ul className="nav">
+        <li><a href="https://www.linkedin.com/in/dhsiao510/" target="_blank" rel="noreferrer">LinkedIn</a></li>
+        <li><a href="https://github.com/dhsiao510" target="_blank" rel="noreferrer">GitHub</a></li>
+        <li><a href="https://twitter.com/dhsiao510" target="_blank" rel="noreferrer">Twitter</a></li>
+      </ul>
+      <div className="main">
+        <h1>David Hsiao</h1>
+        <h3>davidhsiao510@icloud.com</h3>
+      </div>
     </div>
   );
 }
