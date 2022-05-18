cbdf695da81df399b302c2bcf1c728b69e175e7d
dhsiao510 <dhsiao510@gmail.com>
2022-05-17 19:12:42 -0700

    Changed links from plain text to logo images

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React from "react";
-import "./App.css"
+import "./App.css";
+import LinkedInLogo from "./assets/LinkedInLogo";
+import GithubLogo from "./assets/GithubLogo";
+import TwitterLogo from "./assets/TwitterLogo";
 
 function App() {
   return (
     <div className="App">
-      <ul className="nav">
-        <li><a href="https://www.linkedin.com/in/dhsiao510/" target="_blank" rel="noreferrer">LinkedIn</a></li>
-        <li><a href="https://github.com/dhsiao510" target="_blank" rel="noreferrer">GitHub</a></li>
-        <li><a href="https://twitter.com/dhsiao510" target="_blank" rel="noreferrer">Twitter</a></li>
-      </ul>
       <div className="main">
         <h1>David Hsiao</h1>
         <h3>davidhsiao510@icloud.com</h3>
+        <ul className="nav">
+          <li>
+            <a
+              href="https://www.linkedin.com/in/dhsiao510/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <LinkedInLogo />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/dhsiao510"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GithubLogo />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/dhsiao510"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <TwitterLogo />
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
