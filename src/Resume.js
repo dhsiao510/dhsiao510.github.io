5fdbc38bd94c790afc75d86ead0021e4771b24e7
dhsiao510 <dhsiao510@gmail.com>
2022-05-19 10:25:05 -0700

    attempt to fix /resume on prod

--- src/Resume.js
+++ src/Resume.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Resume = () => {
-  window.location.href =
-    "https://drive.google.com/file/d/1JCXXlYvPbPFU38XXVxKSel9uL-2q4sYh/view?usp=sharing";
+  useEffect(() => {
+    window.location.href =
+      "https://drive.google.com/file/d/1JCXXlYvPbPFU38XXVxKSel9uL-2q4sYh/view?usp=sharing";
+  }, []);
+
   return <></>;
 };
 
